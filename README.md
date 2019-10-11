86c71d8df3da2e823ce00a575273a2073a9c2594
oliviaclyde <olivialclyde@gmail.com>
2019-10-11 09:21:59 -0600

    initial

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AboutMe
 Made my first webpage
+
+Taught myself HTML, CSS and some basic Javascript to make a webpage about myself.
+
+Check out https://oliviaclyde.netlify.com
---
+++ webpage/main.js
@@ -0,0 +1,35 @@
+// Javascript file for aboutme.html
+
+
+//Would need to account for any brower compatibility issues from ES5 to ES6. Make backward compatible?
+
+
+function showAlert() {
+    var myText = "Form Submitted Successfully! Thank you!";
+    alert (myText);
+  }
+
+
+function onMouseOver() {
+    document.getElementById("highlight").style.color = "navy";
+};
+
+function onMouseOut() {
+    document.getElementById("highlight").style.color = "black";
+};
+
+function onMouseOver1() {
+    document.getElementById("highlight1").style.color = "orange";
+};
+
+function onMouseOut1() {
+    document.getElementById("highlight1").style.color = "black";
+};
+
+function onMouseOver2() {
+    document.getElementById("highlight2").style.color = "green";
+};
+
+function onMouseOut2() {
+    document.getElementById("highlight2").style.color = "black";
+};
